2e1031f0e54b02bc658a11366c44fd83887f13a7
RafalDziuba <72468641+RafalDziuba@users.noreply.github.com>
2022-06-05 20:51:39 +0200

    Popup add

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,35 @@
-const articleBtn = document.querySelector("#article-btn");
+const articleBtn = document.querySelector(".article-btn");
 
+let clicks = localStorage.getItem("clicks");
 const clickCounter = () => {
-  let clicks = 0;
-  
+  clicks++;
+  localStorage.setItem("clicks", clicks);
+  console.log(clicks);
 };
+
+const toggleModal = () => {
+  const modal = document.querySelector(".modal");
+  const hideBtn = document.querySelector(".hide-modal");
+  const darker = document.querySelector(".darker");
+  modal.classList.add("show-modal");
+  darker.classList.add("darker-active");
+
+  const hideModalBtn = () => {
+    modal.classList.remove("show-modal");
+    darker.classList.remove("darker-active");
+  };
+
+  hideBtn.addEventListener("click", hideModalBtn);
+  darker.addEventListener("click", hideModalBtn);
+
+  showClicksNumber();
+};
+
+const showClicksNumber = () => {
+  const showClicks = document.querySelector(".clicks-number");
+  const clicksNumber = localStorage.getItem("clicks");
+  showClicks.textContent = clicksNumber + ' times';
+};
+
+articleBtn.addEventListener("click", clickCounter);
+articleBtn.addEventListener("click", toggleModal);
